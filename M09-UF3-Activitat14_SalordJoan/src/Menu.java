9992bd121edc72824247680dd42066e1f6d57c16
ausias <ausias@h5-32-VHP-20>
2022-02-17 10:58:02 +0100

    Semi functional program. Offices class output fixed and finished.

--- M09-UF3-Activitat14_SalordJoan/src/Menu.java
+++ M09-UF3-Activitat14_SalordJoan/src/Menu.java
@@ -1,18 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Menu {
-	public static void main (String args[]) {
+	
+	private static ProcessBuilder builder = new ProcessBuilder ("java", "-jar", "Offices.jar");
+	private static Process p;
+	private static InputStream officeResponseInput = null; 
+	private static BufferedReader officeResponse;
+	
+	private static ArrayList<int[]> lottoPlays = new ArrayList<>();
+	private static ArrayList<String> lottoName = new ArrayList<>();
+	
+	public static void main (String args[]) throws IOException {
+		// Offices program start-up
+		p = builder.start();
+		officeResponseInput = p.getInputStream();
+		officeResponse = new BufferedReader(new InputStreamReader(officeResponseInput));
+		
 		menu();
 	}
 	
-	public static void menu () {
+	public static void menu () throws IOException {
 		System.out.println("------ LOTTO EXPRESS ------");
 		System.out.println("1. Add office");
 		System.out.println("2. Winners"); //Add winners on .txt
 		System.out.println("3. Exit");
 		
+		readEntry();
+	}
+	
+	public static void readEntry () throws IOException {
 		Scanner sc = new Scanner(System.in);
 		String entry = sc.nextLine();
 		
+		if (entry.equals("1")) {
+			System.out.println("Entry office name: ");
+			entry = sc.nextLine();
+			// Add argument - Creates an office with the given name.
+			PrintWriter pw = new PrintWriter(p.getOutputStream());
+			pw.println(entry);
+			pw.flush();
+			
+			fill(officeResponse);
+		}
+		menu();
+	}
+	
+	public static void fill (BufferedReader entry) throws IOException {
+		String officeEntry = entry.readLine();
+		System.out.println(officeEntry);
+		String[] play  = officeEntry.split(" ");
+		//System.out.println(play[0] + "--- " + play[2]);
+		String officeName = play[0];
+		lottoName.add(officeName);
+		
+		String numberString = play[1];
+		//System.out.println("+asdf" + numberString);
+		int[] numberInt = new int[7];
+		for (int i = 0; i<numberString.length(); i++) {
+			//numberInt[i] = Integer.parseInt(numberString[i]);
+		}
+		lottoPlays.add(numberInt);
+		
+		for (int i = 0; i<lottoName.size(); i++) {
+			//System.out.println(lottoName.get(i));
+			//System.out.println(Arrays.toString(lottoPlays.get(i)));
+		}
 	}
 }
--- M09-UF3-Activitat14_SalordJoan/src/Offices.java
+++ M09-UF3-Activitat14_SalordJoan/src/Offices.java
@@ -1,14 +1,40 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Offices {
+	
+	private static int[] play = new int[7];
+	private static String officeName = null;
 
 	public static void main (String args[]) {
 		Scanner sc = new Scanner(System.in);
-		String officeName = null;
 		
 		while (sc.hasNext()) {
 			officeName = sc.nextLine();
-			System.out.println("Office name: " + officeName);
+			generateRandom();
 		}
+		sc.close();
 	}
+	
+	 //Generates play number.
+    public static void generateRandom() {
+    	int[] numArray = new int[7];
+        for (int i = 0; i < 7; i++) {
+            int e = (int) ((Math.random() * (49 - 0)) + 0);
+            for (int j = 0 ; j < i; j++) {
+            	if (numArray[j] == e) {
+            		i--;
+            	} else {
+            		numArray[i] = e;
+            	}
+            }
+        }
+        //String t1 = Arrays.toString(numArray);
+        String t1 = "";
+        for (int  i = 0; i<numArray.length; i++) {
+        	t1 += ","+numArray[i];
+        }
+        //t1 = t1.substring(1, t1.length()-1);
+        System.out.println(officeName + " " + t1);
+    }
 }
